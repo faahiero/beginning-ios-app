065e845bb64159b2d87c737e7f146aeca0cea45e
Fabricio Silva <fabriciosilvalp@outlook.com>
2022-06-18 18:02:11 -0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Fabricio Silva on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

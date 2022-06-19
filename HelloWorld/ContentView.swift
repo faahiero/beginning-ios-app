202b8a12da0a173cedf68fc0368e049985691e63
Fabricio Silva <fabriciosilvalp@outlook.com>
2022-06-18 23:10:19 -0300

    Stack Views and Methods

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,24 +9,43 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
+    
+    func speak (text: String){
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
 
     var body: some View {
         
-        Button {
-            let utterance = AVSpeechUtterance(string: "Hello, World")
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-            let synthesizer = AVSpeechSynthesizer()
-            synthesizer.speak(utterance)
-                
-        } label: {
-            Text("Hello World")
-                .fontWeight(.bold)
-                .font(.system(.title, design: .rounded))
-        }
-        .padding()
-        .foregroundColor(.white)
-        .background(Color.purple)
+        VStack {
+            
+            Button {
+                speak(text: "Happy Programming")
+            } label: {
+                Text("Happy Programming")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.yellow)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "Hello World")
+                    
+            } label: {
+                Text("Hello World")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
         .cornerRadius(20)
+        }
     }
 }
 
